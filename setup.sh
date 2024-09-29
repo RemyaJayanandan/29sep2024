8f12656001f71234845b763cffa65d0aecc5b69a
RemyaJayanandan <137802738+RemyaJayanandan@users.noreply.github.com>
2024-09-29 15:59:30 +0300

    Update setup.sh
    added "sudo ./aws/install --update " under install aws cli function

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ install_aws_cli() {
   echo "Installing AWS CLI..."
   curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
   unzip awscliv2.zip
-  sudo ./aws/install
+  sudo ./aws/install --update
   aws --version
   echo "AWS CLI installed successfully."
 }
